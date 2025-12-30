fda61ae7589dd8511bc0258750047021f3f706a6
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-12-30 17:05:57 +0200

    Update 80-cpu-temp.sh

--- bfetch/bfetch-modules/80-cpu-temp.sh
+++ bfetch/bfetch-modules/80-cpu-temp.sh
@@ -5,12 +5,9 @@ module_cpu_temp() {
     if [ -f /sys/class/thermal/thermal_zone0/temp ]; then
         local temp=$(cat /sys/class/thermal/thermal_zone0/temp)
         local celsius=$((temp/1000))
-        echo "°󰔏 CPU:${celsius} °C"
+        echo "°󰔏 CPU:${celsius}°C"
     elif command -v sensors &>/dev/null; then
         local temp=$(sensors | grep -i "core 0" | awk '{print $3}' | tr -d '+°C' | cut -d. -f1)
         [ -n "$temp" ] && echo "  ${temp}°C"
     fi
 }
-
-# Execute if run directly
-module_cpu_temp
